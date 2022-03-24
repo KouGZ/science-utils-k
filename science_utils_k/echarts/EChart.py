fc784953c098ea7b2dd0cbdd18c862eb8946c981
kouguozhao <kouguozhao@163.com>
2022-03-24 23:49:46 +0800

    init

--- science_utils_k/echarts/EChart.py
+++ science_utils_k/echarts/EChart.py
@@ -3,13 +3,27 @@ from abc import abstractclassmethod, abstractmethod
 
 
 class EChart(object):
-    def __init__(self, global_args, options=None):
-        self.global_args = global_args
-    
+    def __init__(self, global_args=None, options=None):
+        if global_args == None:
+            self.global_args = {
+                "id": "chart",
+                "init": {
+                    "dom": "document.getElementById('chart')",
+                    "theme": None,
+                    'options': {
+                        "width": 800,
+                        "height": 600
+                    }
+
+                }
+            }
+        else:
+            self.global_args = global_args
     def get_global_args(self):
         return self.global_args
-    
+
     def set_global_args(self):
         pass
+
     def output(type):
         pass
--- science_utils_k/echarts/LineChart.py
+++ science_utils_k/echarts/LineChart.py
@@ -1,32 +1,55 @@
+from tracemalloc import start
 from science_utils_k.echarts.EChart import EChart
 from science_utils_k.utils import princess
 import time
 import os
 
-
 class LineChart(EChart):
 
-    def __init__(self, global_args, options):
-       super(LineChart, self).__init__(global_args)
-       self.options = options
+    def __init__(self, global_args=None, options=None):
+        super(LineChart, self).__init__(global_args)
+        if options == None:
+            self.options = {
+                'title': {
+                    'text': 'Classification',
+                },
+                'tooltip': {},
+                'legend': {},
+                'xAxis': {
+                    'name': "epoch",
+                    'nameLocation': "end",
+                    'data': None,
+                    'axisLabel': {
+                        'show': 'true'
+                    }
+                },
+                'yAxis': {},
+                'series': None
+            }
+        else:
+            self.options = options
 
     def get_options(self):
         return self.options
 
     def set_options(self, attrs, values):
-       for attr, value in attrs, values:
-           attr = attr.split(".")
-    
-    def output(self,type, o_path="log/" + time.strftime("%Y-%m-%d %H-%M-%S", time.localtime()) ):
+        
+        for attr, value in zip(attrs, values):
+            print(attr)
+            print(value)
+            attr = attr.split(".")
+            princess.set_attr(self.options, attr, value)
+
+    def output(self, type, o_path="log/" + time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())):
         if type == "html":
             if not os.path.exists("log"):
                 os.makedirs("log")
             os.makedirs(o_path+"/html/js/")
-            fd = os.open(o_path+"/html/main.html",os.O_RDWR|os.O_CREAT)
+            fd = os.open(o_path+"/html/main.html", os.O_RDWR | os.O_CREAT)
             os.write(fd, str(princess.echart2html(self)).encode())
             os.close(fd)
-            fd = os.open(o_path+"/html/js/charts_cfg.js",os.O_RDWR|os.O_CREAT|os.O_APPEND)
+            fd = os.open(o_path+"/html/js/charts_cfg.js",
+                         os.O_RDWR | os.O_CREAT | os.O_APPEND)
             os.write(fd, princess.js_init_echart(self).encode())
             os.write(fd, princess.js_init_chartOption(self).encode())
             os.close(fd)
-            
